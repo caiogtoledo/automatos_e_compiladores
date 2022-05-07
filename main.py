c77a0a215551c9649b6b224e2e4bd1793ffb9703
Caio <ctoledo@pieracciani.com.br>
2022-05-07 19:27:16 -0300

    aceitando qualquer arquivo

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import sys
 
 class Dfa:
     def __init__(self):
-        # self.fileName = fileName
-        with open('m.dfa') as dfa_file:
+        file_name = input('Digite o nome do arquivo do autômato: ')
+        with open(file_name) as dfa_file:
             dfa_data = dfa_file.read()
 
         dfa = eval(dfa_data)
@@ -13,7 +13,6 @@ class Dfa:
 
     def run(self):
         program(self.dfa)
-
 
 if __name__ == "__main__":  
     dfa1 = Dfa()
